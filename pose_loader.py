70a04888412d766699eb7a8d6117732c09a5fdac
Juan_Alarc <111084807+RobotJuan@users.noreply.github.com>
2025-04-10 14:18:12 -0400

    Add files via upload
    ARCHIVOS DE BRUNO

---
+++ pose_loader.py
@@ -0,0 +1,23 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class PoseLoader(Node):
+    def __init__(self):
+        super().__init__('carga_pose')
+        self.publisher = self.create_publisher(String, 'goal', 10)
+        with open("goals.txt","r") as f:
+            data = f.readlines()
+        data = [line.strip("\n") for line in data]
+
+        for line in data:
+            msg = String()
+            msg.data = f'{line}'
+            self.publisher.publish(msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = PoseLoader()
+    
+if __name__ == '__main__':
+    main()
