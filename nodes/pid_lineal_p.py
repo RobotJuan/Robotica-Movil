f64611a3487ab7ec9aad5c250e55cf5d016c2af9
brunaldihno <111083391+brunaldihno@users.noreply.github.com>
2025-05-27 19:06:18 -0400

    Update pid_lineal_p.py

--- nodes/pid_lineal_p.py
+++ nodes/pid_lineal_p.py
@@ -17,9 +17,9 @@ class PIDController(Node):
         self.integral = 0
         self.t_anterior = time.time()
 
-        self.setpoint_sub = self.create_subscription(Float64, 'setpoint_lineal_p', self.setpoint_cb, 1)
+        self.setpoint_sub = self.create_subscription(Float64, 'setpoint_lineal', self.setpoint_cb, 1)
         self.state_sub = self.create_subscription(Float64, 'state_lineal', self.state_cb, 1)
-        self.control_pub = self.create_publisher(Float64, 'control_effort_lineal_p', 1)
+        self.control_pub = self.create_publisher(Float64, 'control_effort_lineal', 1)
 
     def setpoint_cb(self, msg):
         self.get_logger().info(f'[P-LINEAL] Nuevo setpoint recibido: {msg.data:.2f}')
