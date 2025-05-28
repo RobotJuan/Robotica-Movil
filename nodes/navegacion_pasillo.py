0f93a89db37c37f90a43b66235aee20b7c095bbc
Juan_Alarc <111084807+RobotJuan@users.noreply.github.com>
2025-05-28 00:02:36 -0400

    Update navegacion_pasillo.py
    actualizado para empezar solo si hay pared, y mejoras del rango visible e inversion de valores

--- nodes/navegacion_pasillo.py
+++ nodes/navegacion_pasillo.py
@@ -10,31 +10,45 @@ from geometry_msgs.msg import Twist, Vector3
 import cv2
 from cv_bridge import CvBridge
 
-class ObstacleDetector(Node):
+class NavegadorPasillo(Node):
 
     def __init__(self):
-        super().__init__("Vigia")
+        super().__init__("Vigia_Pasillo")
         self.publisher = self.create_publisher(Twist, "/cmd_vel_mux/input/navigation", 10)
         self.depth_sub = self.create_subscription(Image, '/camera/depth/image_raw', self.depth_cb, 10 )
         self.bridge = CvBridge()
         self.current_cv_depth_image = "A"
-        self.threshold_value = 0.6
+        self.threshold_value = 0.7
         self.filas = 480
         self.columnas = 640
         self.vel_base = 0.2
-        self.kp = 0.0001
-        self.publisher_ = self.create_publisher(Vector3, "/occupancy_state", 10)
+        self.kp = 0.00005
+        self.iniciado = False
+        # self.publisher_ = self.create_publisher(Vector3, "/occupancy_state", 10)
         
     def depth_cb(self, data):
         depth_image = self.bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
         depth_image = depth_image.astype(np.float32)
         cv2.imshow('imagen original', depth_image)
         cv2.waitKey(1)
-        depth_clean = np.nan_to_num(depth_image, nan=0.9*self.threshold_value)
-        depth_clean = np.where(depth_clean<1*self.threshold_value, depth_clean, 0)
+        depth_clean = np.nan_to_num(depth_image, nan=0.5*self.threshold_value)
+        depth_clean = np.where(depth_clean<1*self.threshold_value, depth_clean, 10)
         aux_sin_piso = depth_clean[:round(0.85*self.filas), :]
-        self.current_cv_depth_image = aux_sin_piso
+        aux_invertida = 1.0 / aux_sin_piso
+        self.current_cv_depth_image = aux_invertida
+
+        aux_clean = np.where(aux_sin_piso<1*self.threshold_value, aux_sin_piso, 0)
+        
+        tot = np.sum(aux_clean)
+        #der = np.sum(aux_clean)
+
+
+        if tot>0:
+            self.iniciado = True
+        
         cv2.imshow('imagen filtrada', aux_sin_piso)
+        cv2.waitKey(1)
+        cv2.imshow('imagen invertida', aux_invertida)
         cv2.waitKey(1)
         print(f"cantidad de elementos NAN, {np.isnan(aux_sin_piso).sum()}")
         self.centrar_posicion()
@@ -48,40 +62,44 @@ class ObstacleDetector(Node):
 
             dist_izq = np.sum(img_aux1)
             dist_der = np.sum(img_aux2)
-            print(f'cant total {alto*ancho}')
+            total = np.sum(self.current_cv_depth_image)
+            print(f'cant total {total}')
             print(f'cant der {dist_der}')
             print(f'cant izq {dist_izq}')
-            error = dist_der-dist_izq
 
-            if error> 0 :
-               
-               print(f" estoy mas cerca de la derecha, error: {error}")
+            if self.iniciado:#(dist_der + dist_izq)>27000:
+                error = dist_der-dist_izq
 
-            elif error <0:
-                print(f" estoy mas cerca de la izquierda, error: {error}")
-        
+                if error> 0 :
+                    print(f" estoy mas cerca de la derecha, error: {error}")
 
-            actuacion = self.kp*error
-            # actuacion = 0
+                elif error <0:
+                    print(f" estoy mas cerca de la izquierda, error: {error}")
+            
 
-            if dist_der + dist_izq < 0.5*alto*ancho: 
-                twist = Twist()
-                twist.linear.x = float(self.vel_base)
-                twist.angular.z = float(actuacion)
-                self.publisher.publish(twist)
-            else:
-                twist= Twist()
-                self.publisher.publish(twist)
+                actuacion = self.kp*error
+                # actuacion = 0
+
+                if dist_der + dist_izq < 0.5*alto*ancho: 
+                    twist = Twist()
+                    twist.linear.x = float(self.vel_base)
+                    twist.angular.z = float(actuacion)
+                    self.publisher.publish(twist)
+                else:
+                    twist= Twist()
+                    self.publisher.publish(twist)
 
 def main(args=None):
   rclpy.init(args=args)
-  obstacle_detec = ObstacleDetector()
-  rclpy.spin(obstacle_detec)
+  pared_detec = NavegadorPasillo()
+  rclpy.spin(pared_detec)
 
-  obstacle_detec.destroy_node()
+  pared_detec.destroy_node()
   rclpy.shutdown()
 
 
 if __name__ == '__main__':
   main()
+
+
 
